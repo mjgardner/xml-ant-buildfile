11e12eaee5f5c618266425cd1acf74edac3a2154
Mark Gardner <gardnerm@gsicommerce.com>
2011-03-30 15:29:23 -0400

    cherry-pick from 70d058a781e8f2700d2cf0211d14dae4a7786b0d

---
+++ lib/XML/Ant/BuildFile/ResourceContainer.pm
@@ -0,0 +1,53 @@
+package XML::Ant::BuildFile::ResourceContainer;
+
+use English '-no_match_vars';
+use Moose;
+use Module::Pluggable (
+    sub_name    => 'resource_plugins',
+    search_path => 'XML::Ant::BuildFile::Resource',
+    require     => 1,
+);
+use Regexp::DefaultFlags;
+## no critic (RequireDotMatchAnything, RequireExtendedFormatting)
+## no critic (RequireLineBoundaryMatching)
+
+=method BUILD
+
+Automatically run after object construction to set up task object support.
+
+=cut
+
+sub BUILD {
+    my $self = shift;
+
+    ## no critic (ValuesAndExpressions::ProhibitMagicNumbers)
+    my %isa_map = map { lc( ( split /::/ => $ARG )[-1] ) => $ARG }
+        $self->project->resource_plugins;
+    $self->meta->add_attribute(
+        _tasks => (
+            traits      => [qw(XPathObjectList Array)],
+            xpath_query => join( q{|} => map {".//$ARG"} keys %isa_map ),
+            isa_map     => \%isa_map,
+            handles     => {
+                all_resources    => 'elements',
+                resource         => 'get',
+                filter_resources => 'grep',
+                num_resources    => 'count',
+            },
+        )
+    );
+    return;
+}
+
+=method resources
+
+Given one or more resource type names, returns a list of objects.
+
+=cut
+
+sub resources {
+    my ( $self, @names ) = @ARG;
+    return $self->filter_resources( sub { $ARG->resource_name ~~ @names } );
+}
+
+1;

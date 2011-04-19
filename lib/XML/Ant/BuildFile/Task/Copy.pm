2a70d31fe51bb36fa867cc1254a9488537f665a5
Mark Gardner <gardnerm@gsicommerce.com>
2011-04-19 16:26:15 -0400

    XML::Ant::BuildFile::Task::Copy::to_dir

--- lib/XML/Ant/BuildFile/Task/Copy.pm
+++ lib/XML/Ant/BuildFile/Task/Copy.pm
@@ -13,25 +13,35 @@ use namespace::autoclean;
 extends 'XML::Ant::BuildFile::ResourceContainer';
 with 'XML::Ant::BuildFile::Task';
 
-has _to_file =>
-    ( ro,
-    ## no critic (ValuesAndExpressions::RequireInterpolationOfMetachars)
-    isa         => Str,
-    traits      => ['XPathValue'],
-    xpath_query => './@tofile',
-    );
-
 =attr to_file
 
 The file to copy to as a L<Path::Class::File|Path::Class::File> object.
 
+=attr to_dir
+
+The directory to copy a set of
+L<XML::Ant::BuildFile::Resource|XML::Ant::BuildFile::Resource>s to as a
+L<Path::Class::Dir|Path::Class::Dir> object.
+
 =cut
 
-has to_file => ( ro, lazy,
-    isa => File,
-    default =>
-        sub { dir( XML::Ant::Properties->apply( $ARG[0]->_to_file ) ) },
-);
+for my $attr (qw(dir file)) {
+    has "_to_$attr" => ( ro,
+        isa         => Str,
+        traits      => ['XPathValue'],
+        xpath_query => "./\@to$attr",
+    );
+
+    has "to_$attr" => ( ro, lazy,
+        isa     => "Path::Class::\u$attr",
+        default => sub {
+            my $method  = "_to_$attr";
+            my $applied = XML::Ant::Properties->apply( $ARG[0]->$method );
+            ## no critic (ProhibitStringyEval, RequireCheckingReturnValueOfEval)
+            return eval "Path::Class::$attr('$applied')";
+        },
+    );
+}
 
 1;
 
--- t/copy_filelist.t
+++ t/copy_filelist.t
@@ -1,12 +1,18 @@
 #!perl
 
-use Test::Most tests => 3;
+use Test::Most tests => 4;
 use English '-no_match_vars';
 use XML::Ant::BuildFile::Project;
 
 my $project = XML::Ant::BuildFile::Project->new( file => 't/yui-build.xml' );
 my $copy = ( $project->target('move-files')->tasks('copy') )[0];
 isa_ok( $copy, 'XML::Ant::BuildFile::Task::Copy', 'copy task' );
+
+is( $copy->to_dir->stringify(),
+    XML::Ant::Properties->get('basedir'),
+    'copy to_dir',
+);
+
 my $filelist = ( $copy->resources('filelist') )[0];
 isa_ok(
     $filelist,
